f541f0c23731ec27686df0d63fcb9148e8a25f7f
Hawkeye <136079895+fakeanonymoususer@users.noreply.github.com>
2023-07-27 21:39:50 +0200

    Add files via upload
    Slight update to HTML. Slight update to style. Upgrade to JS.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Click button, makes modal div */
+/* Click button, makes modal div, I like it 100% */
 function toggleModal(modalId) {
   var modal = document.getElementById(modalId);
   if (modal.style.display === 'block') {
@@ -12,7 +12,7 @@ function toggleModal(modalId) {
     }
   };
 }
-/* Adds listener to header, to explain some stuff when needed */
+/* Adds listener to header, to explain some stuff when needed, done 100%? */
 function toggleTableRows(event) {
   const thead = event.currentTarget;
   const trElements = thead.querySelectorAll("tr");
@@ -28,70 +28,7 @@ tables.forEach((table) => {
   });
 });
 
-/* Highlighting */
-(function() {
-  'use strict';
-
-  var highlightedRows = new Set();
-
-  function highlightRelatedRows(event) {
-    var hoveredRow = event.target.closest('tr');
-    if (!hoveredRow) return;
-
-    var parentTable = hoveredRow.closest('table');
-    if (!parentTable) return;
-
-    if (parentTable.contains(hoveredRow) && parentTable.tHead.contains(hoveredRow)) {
-      return;
-    }
-
-    var rowIndex = Array.from(hoveredRow.parentNode.children).indexOf(hoveredRow);
-
-    var tables = document.getElementsByTagName('table');
-    Array.from(tables).forEach(function(table) {
-      var tbodies = table.getElementsByTagName('tbody');
-      Array.from(tbodies).forEach(function(tbody) {
-        var rows = tbody.getElementsByTagName('tr');
-        Array.from(rows).forEach(function(row, index) {
-          if (index === rowIndex) {
-            row.classList.add('highlighted');
-            highlightedRows.add(row);
-          } else {
-            row.classList.remove('highlighted');
-            highlightedRows.delete(row);
-          }
-        });
-      });
-    });
-  }
-
-  function highlightPage(event) {
-    var hoveredRow = event.target.closest('tr');
-    if (hoveredRow === event.currentTarget) {
-      document.body.classList.add('highlighted-page');
-    } else {
-      document.body.classList.remove('highlighted-page');
-    }
-  }
-
-  function removeHighlight() {
-    highlightedRows.forEach(function(row) {
-      row.classList.remove('highlighted');
-    });
-
-    highlightedRows.clear();
-
-    document.body.classList.remove('highlighted-page');
-  }
-
-  document.addEventListener('DOMContentLoaded', function() {
-    document.body.addEventListener('mouseover', highlightRelatedRows);
-    document.body.addEventListener('mouseout', removeHighlight);
-    document.body.addEventListener('mouseenter', highlightPage);
-    document.body.addEventListener('mouseleave', highlightPage);
-  });
-})();
-
+/* Highlighting columns, Looks danzo */
 (function() {
   'use strict';
 
@@ -109,7 +46,7 @@ tables.forEach((table) => {
       tbodyRows[i].classList.toggle('highlighted', event.type === 'mouseenter');
     }
   }
-
+  /* If you are reading this you owe me 20 billion pretzels, now get out! */
   document.addEventListener('DOMContentLoaded', function() {
     var theads = document.getElementsByTagName('thead');
     for (var i = 0; i < theads.length; i++) {
@@ -118,142 +55,8 @@ tables.forEach((table) => {
     }
   });
 })();
-/* */
 
-(function(document) {
-  'use strict';
-  console.log("script.js loaded");
-  /* Table search JS */
-  var LightTableFilter = (function(Arr) {
-    let _input;
-    let _originalRows = new Map();
-
-    function _onInputEvent(e) {
-      _input = e.target;
-      var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
-      Arr.forEach.call(tables, function(table) {
-        var tbody = table.querySelector('tbody');
-
-        if (tbody === null) {
-          return; 
-        }
-
-        if (!_originalRows.has(table)) {
-          _originalRows.set(table, Array.from(tbody.rows));
-        }
-
-        if (_input.value === '') {
-          _restoreOriginalRows(table, tbody);
-        } else {
-          _filterRows(tbody, _input.value.toLowerCase());
-          _checkParentTableVisibility(table); 
-        }
-      });
-    }
-
-    function _filterRows(tbody, filterValue) {
-      var visibleRows = [];
-      Arr.forEach.call(tbody.rows, function(row) {
-        var text = row.textContent.toLowerCase();
-        var isValid = text.indexOf(filterValue) !== -1;
-
-        if (isValid) {
-          // Restore the row if it matches the search term
-          row.style.display = 'table-row';
-          visibleRows.push(row);
-        } else {
-          // Check for exemption class in ancestor nodes of the row
-          var ancestor = row.parentElement;
-          var isExempt = false;
-          while (ancestor) {
-            if (ancestor.nodeType === Node.ELEMENT_NODE && ancestor.classList.contains('exemption')) {
-              isExempt = true;
-              break;
-            }
-            ancestor = ancestor.parentElement;
-          }
-
-          if (!isExempt) {
-            row.style.display = 'none';
-          }
-        }
-      });
-
-      tbody.innerHTML = '';
-      visibleRows.forEach(function(row) {
-        tbody.appendChild(row);
-      });
-    }
-
-    function _checkParentTableVisibility(table) {
-      var tbody = table.querySelector('tbody');
-      if (tbody === null) {
-        return;
-      }
-
-      var rows = tbody.querySelectorAll('tr');
-      var allRowsHidden = true;
-
-      Arr.forEach.call(rows, function(row) {
-        if (row.style.display !== 'none') {
-          allRowsHidden = false;
-          return;
-        }
-      });
-
-      if (allRowsHidden) {
-        table.style.display = 'none';
-      } else {
-        table.style.display = 'flex';
-      }
-    }
-
-    function _restoreOriginalRows(table, tbody) {
-      if (tbody === null) {
-        return;
-      }
-
-      if (!_originalRows.has(table)) {
-        return;
-      }
-
-      tbody.innerHTML = ''; // Clear current tbody content
-      _originalRows.get(table).forEach(function(originalRow) {
-        originalRow.style.display = 'table-row'; // Ensure the row is visible when restored
-        tbody.appendChild(originalRow.cloneNode(true)); // Restore the original rows (cloning to avoid reference issues)
-      });
-
-      _checkParentTableVisibility(table); // Check and hide parent table if needed
-    }
-
-    return {
-      init: function() {
-        var inputs = document.getElementsByClassName('light-table-filter');
-        Arr.forEach.call(inputs, function(input) {
-          input.oninput = _onInputEvent;
-
-          // Listen to the input event on the search input to check if the input value is empty
-          input.addEventListener('input', function() {
-            var table = document.querySelector(input.getAttribute('data-table'));
-            var tbody = table ? table.querySelector('tbody') : null;
-            if (input.value === '') {
-              _restoreOriginalRows(table, tbody);
-            }
-          });
-        });
-      },
-      _restoreOriginalRows: _restoreOriginalRows // Make _restoreOriginalRows accessible outside the closure
-    };
-  })(Array.prototype);
-
-  document.addEventListener('readystatechange', function() {
-    if (document.readyState === 'complete') {
-      LightTableFilter.init();
-    }
-  });
-})(document);
-
-
+/* Which trackers you can and cannot use the client on, this should be done 100% */
 function updateTableStatus() {
   const notViableParagraph = document.getElementById('not_viable');
   const yesViableParagraph = document.getElementById('yes_viable');
@@ -277,7 +80,6 @@ function updateTableStatus() {
     }
   }
 
-  // Update paragraphs initially
   updateParagraphs();
 
   // Observer configuration
@@ -285,8 +87,6 @@ function updateTableStatus() {
     attributes: true,
     attributeFilter: ['style'],
   };
-
-  // Callback function to handle DOM changes
   const observerCallback = function (mutationsList) {
     for (const mutation of mutationsList) {
       if (mutation.target.classList.contains('order-table')) {
@@ -295,10 +95,112 @@ function updateTableStatus() {
       }
     }
   };
-
-  // Create and start the MutationObserver
   const observer = new MutationObserver(observerCallback);
   tables.forEach((table) => observer.observe(table, observerConfig));
 }
 
 updateTableStatus();
+
+/* search and destroy, working as I want it to! Done */
+(function(document) {
+  'use strict';
+  console.log("script.js loaded");
+
+  /* Table search JS */
+  const LightTableFilter = (function(Arr) {
+    let _input;
+    const _originalRows = new Map();
+
+    function _onInputEvent(e) {
+      _input = e.target;
+      const tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+      Arr.forEach.call(tables, function(table) {
+        if (table.classList.contains('exemption')) {
+          return;
+        }
+
+        const tbody = table.querySelector('tbody');
+        if (tbody === null) return;
+
+        if (!_originalRows.has(table)) {
+          _originalRows.set(table, Array.from(tbody.rows));
+        }
+
+        const filterValue = _input.value.toLowerCase();
+        _filterRows(tbody, filterValue);
+        _checkParentTableVisibility(table);
+        _updateOddEvenClasses(tbody);
+      });
+    }
+
+    function _filterRows(tbody, filterValue) {
+      Arr.forEach.call(tbody.rows, function(row) {
+        const text = row.textContent.toLowerCase();
+        const isValid = text.indexOf(filterValue) !== -1;
+
+        row.style.display = isValid ? 'table-row' : 'none';
+      });
+    }
+
+    function _checkParentTableVisibility(table) {
+      const tbody = table.querySelector('tbody');
+      if (!tbody) return;
+
+      const rows = tbody.querySelectorAll('tr');
+      let allRowsHidden = true;
+
+      Arr.forEach.call(rows, function(row) {
+        if (row.style.display !== 'none') {
+          allRowsHidden = false;
+          return;
+        }
+      });
+
+      table.style.display = allRowsHidden ? 'none' : 'flex';
+    }
+
+    function _restoreOriginalRows(tbody) {
+      const table = tbody.closest('table');
+      if (!table || !_originalRows.has(table)) {
+        return;
+      }
+
+      const originalRows = _originalRows.get(table);
+      tbody.innerHTML = '';
+      originalRows.forEach(function(originalRow) {
+        originalRow.style.display = 'table-row';
+        tbody.appendChild(originalRow.cloneNode(true));
+      });
+
+      _checkParentTableVisibility(table);
+      _updateOddEvenClasses(tbody);
+    }
+
+    function _updateOddEvenClasses(tbody) {
+      const visibleRows = tbody.querySelectorAll('tr[style="display: table-row;"]');
+      Arr.forEach.call(visibleRows, function(row, index) {
+        row.classList.remove('odd', 'even');
+        row.classList.add(index % 2 === 0 ? 'even' : 'odd');
+      });
+    }
+
+    return {
+      init: function() {
+        const inputs = document.getElementsByClassName('light-table-filter');
+        Arr.forEach.call(inputs, function(input) {
+          input.oninput = _onInputEvent;
+          input.addEventListener('input', _onInputEvent); // Trigger filtering when input changes (including character deletion)
+        });
+      },
+      _restoreOriginalRows: _restoreOriginalRows,
+      _filterRows: _filterRows,
+      _checkParentTableVisibility: _checkParentTableVisibility
+    };
+  })(Array.prototype);
+
+  document.addEventListener('readystatechange', function() {
+    if (document.readyState === 'complete') {
+      LightTableFilter.init();
+    }
+  });
+})(document);
